0b3983282845cb0bc29be7c7c41c55e46e295f8c
Aleksandar Brkljac <aleksandar.brkljac@vega.local>
2021-11-04 17:50:57 +0100

    feat: Connection to postgreSQL

--- src/DrugstoreApp/Backend/DrugstoreAPI/Drugstore/Models/MyDbContext.cs
+++ src/DrugstoreApp/Backend/DrugstoreAPI/Drugstore/Models/MyDbContext.cs
@@ -15,9 +15,9 @@ namespace Drugstore.Models
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Medicine>().HasData(
-                new Medicine { Id = 0, Name = "Brufen", Price = 250.00 },
-                new Medicine { Id = 1, Name = "Olynth", Price = 180.00 },
-                new Medicine { Id = 2, Name = "Polinol", Price = 670.00 }
+                new Medicine { Id = 1, Name = "Brufen", Price = 250.00 },
+                new Medicine { Id = 2, Name = "Olynth", Price = 180.00 },
+                new Medicine { Id = 3, Name = "Polinol", Price = 670.00 }
             );
         }
     }
