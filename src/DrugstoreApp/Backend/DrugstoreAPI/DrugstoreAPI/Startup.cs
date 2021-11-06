2673cf3d432d9fa756d634b26f3b6d1b0ec66e21
dusansisarica <74587121+dusansisarica@users.noreply.github.com>
2021-11-06 15:02:35 +0100

    feat: #3 Setup initial project for drugstore application
    * feat: #3 Setup initial project for drugstore application
    
    * feat: #3 Setup initial project for drugstore application
    
    * feat: Created test schedule
    
    * feat: Created test schedule (#5)
    
    * feat: Connection to postgreSQL
    
    * feat: #3 Setup initial project controller
    
    Co-authored-by: BogdanaZivkovic <zivkovic.ra2.2018@uns.ac.rs>
    Co-authored-by: lenche23 <aleksic.ra23.2018@uns.ac.rs>
    Co-authored-by: Aleksandar Brkljac <aleksandar.brkljac@vega.local>
    Co-authored-by: Aleksandar <73308871+Aleksandar9999@users.noreply.github.com>

--- src/DrugstoreApp/Backend/DrugstoreAPI/DrugstoreAPI/Startup.cs
+++ src/DrugstoreApp/Backend/DrugstoreAPI/DrugstoreAPI/Startup.cs
@@ -1,14 +1,10 @@
+using Drugstore.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace DrugstoreAPI
 {
@@ -25,6 +21,8 @@ namespace DrugstoreAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<MyDbContext>(options =>
+            options.UseNpgsql(ConfigurationExtensions.GetConnectionString(Configuration, "MyDbContextConnectionString")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
