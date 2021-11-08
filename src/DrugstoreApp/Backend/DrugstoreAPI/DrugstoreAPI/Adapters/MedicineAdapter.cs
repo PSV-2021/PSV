af4b1d598be83af6de7e6ccc77a998ce5d9da41e
Aleksandar Brkljac <aleksandar.brkljac@vega.local>
2021-11-08 16:21:39 +0100

    fix: Created losed classes

---
+++ src/DrugstoreApp/Backend/DrugstoreAPI/DrugstoreAPI/Adapters/MedicineAdapter.cs
@@ -0,0 +1,25 @@
+﻿
+using Drugstore.Models;
+using DrugstoreAPI.Dtos;
+
+namespace DrugstoreAPI.Adapters
+{
+    public class MedicineAdapter
+    {
+        public static Medicine MedicineDtoToMedicine(MedicineDto dto)
+        {
+            Medicine medicine = new Medicine();
+            medicine.Name = dto.Name;
+            medicine.Price = dto.Price;
+            return medicine;
+        }
+
+        public static MedicineDto MedicineToMedicineDto(Medicine medicine)
+        {
+            MedicineDto dto = new MedicineDto();
+            dto.Name = medicine.Name;
+            dto.Price = medicine.Price;
+            return dto;
+        }
+    }
+}
---
+++ src/DrugstoreApp/Backend/DrugstoreAPI/DrugstoreAPI/Dtos/MedicineDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrugstoreAPI.Dtos
+{
+    public class MedicineDto
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public MedicineDto() { }
+    }
+}
