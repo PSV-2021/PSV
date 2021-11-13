f51fbf05552b2b10a008423def7dfe71975037d0
masa-bursac <73540273+masa-bursac@users.noreply.github.com>
2021-11-13 12:15:05 +0100

    feat: Initalization of test project
    Created xUnit project for API

---
+++ src/HospitalAPI/HospitalAPI/HospitalApiTests/Constants.cs
@@ -0,0 +1,10 @@
+using System;
+using Xunit;
+
+namespace HospitalApiTests
+{
+    public class Constants
+    {
+        public static string ConnectionString => "server=localhost; port=5432; database=hospital; User Id = postgres; password=masa3009";
+    }
+}
