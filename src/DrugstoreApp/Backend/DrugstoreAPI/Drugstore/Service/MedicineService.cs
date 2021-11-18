0728b796ed8a248488d87ab7c8f2c5585c66f153
dusansisarica <74587121+dusansisarica@users.noreply.github.com>
2021-11-18 17:22:27 +0100

    feat: #16 Inventory management

--- src/DrugstoreApp/Backend/DrugstoreAPI/Drugstore/Service/MedicineService.cs
+++ src/DrugstoreApp/Backend/DrugstoreAPI/Drugstore/Service/MedicineService.cs
@@ -1,20 +1,43 @@
 ﻿using Drugstore.Repository.Interfaces;
 using Drugstore.Models;
 using Drugstore.Repository.Sql;
+using System.Collections.Generic;
 
 namespace DrugstoreAPI.Service
 {
     public class MedicineService
     {
         public IMedicineRepository MedicineRepository { get; set; }
+        public readonly MyDbContext dbContext;
         public MedicineService(IMedicineRepository medicineRepository)
         {
             MedicineRepository = medicineRepository;
         }
 
-        public MedicineService()
+        public MedicineService(MyDbContext context)
         {
-            MedicineRepository = new MedicineSqlRepository();
+            this.dbContext = context;
+            MedicineRepository = new MedicineSqlRepository(context);
+        }
+
+        public List<Medicine> GetAll()
+        {
+            return MedicineRepository.GetAll();
+        }
+
+        public Medicine GetOne(int id)
+        {
+            return MedicineRepository.GetOne(id);
+        }
+
+        public void Add(Medicine medicine)
+        {
+            MedicineRepository.Add(medicine);
+        }
+
+        public bool Delete(int id)
+        {
+            return MedicineRepository.Delete(id);
         }
 
         public bool CheckForAmountOfDrug(string nameOfDrug, int amountOfDrug)
@@ -39,6 +62,12 @@ namespace DrugstoreAPI.Service
         private void DecreaseDrugAmount(int amountOfDrug, Medicine med)
         {
             med.Supply -= amountOfDrug;
+            MedicineRepository.Update(med);
+        }
+
+        private void IncreaseDrugAmount(int amountOfDrug, Medicine med)
+        {
+            med.Supply += amountOfDrug;
             MedicineRepository.Update(med);
         }
     }
