5812dfa2c035015a8fe2aea47e9620a86bddee82
lenche23 <aleksic.ra23.2018@uns.ac.rs>
2021-11-20 20:16:56 +0100

    feat: #13 minor change

--- src/DrugstoreApp/Backend/DrugstoreAPI/Drugstore/Models/Medicine.cs
+++ src/DrugstoreApp/Backend/DrugstoreAPI/Drugstore/Models/Medicine.cs
@@ -14,9 +14,10 @@ namespace Drugstore.Models
         public String SideEffects { get; set; }
         public String Reactions { get; set; }
         public String Usage { get; set; }
-        public List<Medicine> compatibleMedicines { get; set; }
+        public List<Medicine> CompatibleMedicines { get; set; }
         public Double Weight { get; set; }
         public String Precautions { get; set; }
+        public String Substances { get; set; }
 
         public Medicine(int id, String name, double price, int supply)
         {
@@ -26,7 +27,7 @@ namespace Drugstore.Models
             this.Supply = supply;
         }
 
-        public Medicine(int id, string name, double price, int supply, string manufacturer, string sideEffects, string reactions, string usage, List<Medicine> compatibleMedicines, double weight, string precautions) 
+        public Medicine(int id, string name, double price, int supply, string manufacturer, string sideEffects, string reactions, string usage, List<Medicine> compatibleMedicines, double weight, string precautions, string substances) 
         {
             Id = id;
             Name = name;
@@ -36,7 +37,7 @@ namespace Drugstore.Models
             SideEffects = sideEffects;
             Reactions = reactions;
             Usage = usage;
-            this.compatibleMedicines = compatibleMedicines;
+            CompatibleMedicines = compatibleMedicines;
             Weight = weight;
             Precautions = precautions;
             Substances = substances;
