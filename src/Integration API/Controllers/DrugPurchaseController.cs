5b766c7ba83186d8236fd996790f11d4fc8c95aa
lazar-stojcevic <stojcevic.ra84.2018@uns.ac.rs>
2021-12-10 13:20:13 +0100

    fix: Remove forgotten reference

--- src/Integration API/Controllers/DrugPurchaseController.cs
+++ src/Integration API/Controllers/DrugPurchaseController.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Hospital.Medicines.Model;
 using Integration.Model;
 using Integration_API.DTOs;
 using Model.DataBaseContext;
