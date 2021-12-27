b4f2914b8b7211f21d97da552da4940be971eb6f
Tatjana Dukovski <dukovski.ra11.2018@uns.ac.rs>
2021-12-27 16:57:27 +0100

    feat: #E1F2-4 Image viewing in medical record

--- src/PatientApp/Frontend/PatientApp/src/app/medical-record/medical-record.component.ts
+++ src/PatientApp/Frontend/PatientApp/src/app/medical-record/medical-record.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { MedicalRecordService } from '../service/medical-record.service';
 
 @Component({
@@ -9,15 +10,18 @@ import { MedicalRecordService } from '../service/medical-record.service';
 export class MedicalRecordComponent implements OnInit {
 
   medicalRecord: any;
-
-  constructor(private medicalRecordService: MedicalRecordService) { }
+  imagePath: any;
+  constructor(private medicalRecordService: MedicalRecordService, private domSanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
     
-    this.medicalRecordService.GetMedicalRecord('2').subscribe((data: any)=>{
-        console.log(data);
+    this.medicalRecordService.GetMedicalRecord('3').subscribe((data: any)=>{
+        this.imagePath ="data:image/png;base64,"+ atob(data.image);
         this.medicalRecord = data;
     });
   }
+  public getSantizeUrl(url : string) {
+    return this.domSanitizer.bypassSecurityTrustUrl(url);
+}
 
 }
