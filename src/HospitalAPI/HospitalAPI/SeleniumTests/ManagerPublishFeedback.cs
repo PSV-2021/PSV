6016917d9f5036237d07ffe9bfea092aa6a2a9f5
Tatjana Dukovski <dukovski.ra11.2018@uns.ac.rs>
2022-01-10 18:08:56 +0100

    feat: #E7 Manager publishes feedback tests

---
+++ src/HospitalAPI/HospitalAPI/SeleniumTests/ManagerPublishFeedback.cs
@@ -0,0 +1,71 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumTests.Pages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Xunit;
+
+namespace SeleniumTests
+{
+    public class ManagerPublishFeedback
+    {
+        private readonly IWebDriver Driver;
+        private FeedbacksPage FeedbackPage;
+        private FeedbacksPage FeedbackDeclinePage;
+
+
+        public ManagerPublishFeedback()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("start-maximized");
+            options.AddArguments("disable-infobars");
+            options.AddArguments("--disable-extensions");
+            options.AddArguments("--disable-gpu");
+            options.AddArguments("--disable-dev-shm-usage");
+            options.AddArguments("--no-sandbox");
+            options.AddArguments("--disable-notifications");
+
+            Driver = new ChromeDriver(options);
+
+            FeedbackPage = new FeedbacksPage(Driver);
+            FeedbackPage.Navigate();
+
+            FeedbackDeclinePage = new FeedbacksPage(Driver);
+            FeedbackDeclinePage.Navigate();
+        }
+        public void Dispose()
+        {
+            Driver.Quit();
+            Driver.Dispose();
+        }
+        [Fact]
+        public void Approve_Feedback()
+        {
+            FeedbackPage.WaitToLoad();
+            Assert.True(FeedbackPage.ApproveButtonDisplayed());
+            FeedbackPage.ApproveFeedback();
+            FeedbackPage.Navigate();
+            Assert.True(FeedbackPage.ReturnButtonDisplayed());
+
+            Assert.Equal(Driver.Url, FeedbacksPage.URI);
+            Dispose();
+        }
+
+        [Fact]
+        public void Return_Feedback()
+        {
+            FeedbackDeclinePage.WaitToLoad();
+            if (FeedbackDeclinePage.ReturnButtonDisplayed())
+            {
+                FeedbackDeclinePage.ApproveFeedback();
+                FeedbackDeclinePage.Navigate();
+                Assert.True(FeedbackDeclinePage.ApproveButtonsDisplayed());
+            }
+            Assert.Equal(Driver.Url, FeedbacksPage.URI);
+
+            Dispose();
+        }
+    }
+}
---
+++ src/HospitalAPI/HospitalAPI/SeleniumTests/Pages/FeedbacksPage.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumTests.Pages
+{
+    public class FeedbacksPage
+    {
+        private readonly IWebDriver Driver;
+        public const string URI = "http://localhost:3001/feedbacks";
+        private IWebElement ApproveButton => Driver.FindElement(By.XPath("/html/body/app-root/main/app-feedbacks/div/div[1]/div/mat-card/mat-card-content/button/span[1]"));
+        private IWebElement FirstApproveButton => Driver.FindElement(By.XPath("/html/body/app-root/main/app-feedbacks/div/div[1]/div/mat-card/mat-card-content/button/span[1]"));
+        private IWebElement SecondApproveButton => Driver.FindElement(By.XPath("/html/body/app-root/main/app-feedbacks/div/div[2]/div/mat-card/mat-card-content/button/span[1]"));
+        private IWebElement ThirdApproveButton => Driver.FindElement(By.XPath("/html/body/app-root/main/app-feedbacks/div/div[3]/div/mat-card/mat-card-content/button/span[1]"));
+
+        private IWebElement ReturnButton => Driver.FindElement(By.XPath("/html/body/app-root/main/app-feedbacks/div/div[2]/div/mat-card/mat-card-content/button/span[1]"));
+      
+
+        public FeedbacksPage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public void ApproveFeedback() {
+            ApproveButton.Click();
+        }
+        public void Navigate()
+          => Driver.Navigate().GoToUrl(URI);
+
+        public bool ApproveButtonDisplayed()
+        {
+            return ApproveButton.Text.Contains("Approve");
+        }
+        public bool ApproveButtonsDisplayed()
+        {
+            if (FirstApproveButton.Text.Contains("Approve") || SecondApproveButton.Text.Contains("Approve") || ThirdApproveButton.Text.Contains("Approve"))
+                return true;
+            return false;
+        }
+
+        public bool ReturnButtonDisplayed()
+        {
+            if (FirstApproveButton.Text.Contains("Return") || SecondApproveButton.Text.Contains("Return") || ThirdApproveButton.Text.Contains("Return"))
+                return true;
+            return false;
+        }
+        public void WaitToLoad()
+        {
+            var wait = new WebDriverWait(Driver, new TimeSpan(0, 0, 30));
+            wait.Until(condition => ApproveButton.Displayed
+                );
+        }
+        public void ReturnButtonIsDisplayed()
+        {
+            var wait = new WebDriverWait(Driver, new TimeSpan(0, 0, 30));
+            wait.Until(condition =>
+            {
+                try
+                {
+                    return ReturnButton.Displayed;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+        }
+    }
+}
