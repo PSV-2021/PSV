d0e4df4226f3896843b7877cd1015bf96f4044ee
NatasaVasicRA10 <73425626+NatasaVasicRA10@users.noreply.github.com>
2022-01-12 15:53:24 +0100

    fix: #E7 fixing selenium test parametrization (#120)
    * feat: #7/run static code analysis 
    
    * feat: Created test schedule (#5)
    
    * feat: #7 Created sonar project properties file
    
    Co-authored-by: Aleksandar Brkljac
    
    * fix: #E7 fixed parametrization
    
    Co-authored-by: Aleksandar <73308871+Aleksandar9999@users.noreply.github.com>

--- src/HospitalAPI/HospitalAPI/SeleniumTests/Configurations.cs
+++ src/HospitalAPI/HospitalAPI/SeleniumTests/Configurations.cs
@@ -8,7 +8,7 @@ namespace SeleniumTests
     {
         public static string HostPatient
         {
-            get => Environment.GetEnvironmentVariable("MF_HOST") ?? "http://localhost:4200";
+            get => Environment.GetEnvironmentVariable("PF_HOST") ?? "http://localhost:4200";
         }
 
         public static string HostManager
