c4dc52ae00fcbd7a03068e7fe20319a98dacffc8
mihailomajstorovic47 <73445435+mihailomajstorovic47@users.noreply.github.com>
2022-01-24 00:32:47 +0100

    feat: #US#17/Tender checks

--- src/IntegrationApp/Integration/Tendering/Model/DrugTender.cs
+++ src/IntegrationApp/Integration/Tendering/Model/DrugTender.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace Integration.Tendering.Model
 {
@@ -14,19 +15,34 @@ namespace Integration.Tendering.Model
         public DrugTender(string id, DateTime tenderEnd, string tenderInfo, bool isFinished)
         {
             Id = id;
-            TenderEnd = tenderEnd;
-            TenderInfo = tenderInfo;
+            TenderEnd = IsTenderEndValid(tenderEnd) ? tenderEnd : DateTime.Now.AddDays(10);
+            TenderInfo = IsDrugFormatValid(tenderInfo) ? tenderInfo : "";
             this.isFinished = isFinished;
         }
 
         public DrugTender(DateTime tenderEnd, string tenderInfo, bool isFinished)
         {
-            TenderEnd = tenderEnd;
-            TenderInfo = tenderInfo;
+            TenderEnd = IsTenderEndValid(tenderEnd) ? tenderEnd : DateTime.Now.AddDays(10);
+            TenderInfo = IsDrugFormatValid(tenderInfo) ? tenderInfo : "";
             this.isFinished = isFinished;
         }
         public DrugTender()
         {
+        }
+
+        private bool IsDrugFormatValid(string info)
+        {
+            Regex MyPattern = new Regex(@"(([a-zA-Z0-9\s]*) - ([0-9]{1,},{0,1})){1,}");
+            if (MyPattern.IsMatch(info))
+                return true;
+            return false;
+        }
+
+        private bool IsTenderEndValid(DateTime tenderEnd)
+        {
+            if (tenderEnd < DateTime.Now)
+                return true;
+            return false;
         }
     }
 }
