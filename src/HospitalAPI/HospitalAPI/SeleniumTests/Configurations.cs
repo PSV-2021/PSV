df99a8b95f047124274e40b7a9f68199119a6c9f
Aleksandar Brkljac <aleksandar.brkljac@vega.local>
2022-01-23 23:56:50 +0100

    try to run remote e2e tests with remote web driver

--- src/HospitalAPI/HospitalAPI/SeleniumTests/Configurations.cs
+++ src/HospitalAPI/HospitalAPI/SeleniumTests/Configurations.cs
@@ -8,7 +8,7 @@ namespace SeleniumTests
     {
         public static string HostPatient
         {
-            get => Environment.GetEnvironmentVariable("PF_HOST") ?? "http://localhost:4200";
+            get => Environment.GetEnvironmentVariable("PF_HOST") ?? "http://localhost:4201";
         }
 
         public static string HostManager
